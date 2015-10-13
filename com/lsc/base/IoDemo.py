bd7b2af0ac5b5e500cbd3a14d2ef01340d59d1f4
lsc <by-lsc@foxmail.com>
2015-10-13 17:30:23 +0800

    io

---
+++ com/lsc/base/IoDemo.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*
+'''
+Created on 2015年10月13日
+
+@author: lsc
+'''
+import os
+
+
+def copyTxtFile(s,d,m):
+    try:
+        f = open(s,m);
+        #print(f.read())
+        w = open(d,'wb+')
+        w.write(f.read())
+        print(".......................\n",w.read())
+    except Exception as e:
+        print(e)
+    finally:
+        f.close()
+        w.close()
+
+    
+#copyTxtFile("IOdemo.txt","dist.txt","r")
+copyTxtFile("pic.jpg","pic1.jpg","rb") 
+print(os.name)  
+os.rename('dist.txt', '111.txt')
+os.remove('111.txt')
+
+
+
+
