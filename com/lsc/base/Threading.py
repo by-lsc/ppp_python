6af4829a953e5a952c87bf93dcc881d1e90cece7
lsc <by-lsc@foxmail.com>
2015-10-15 17:41:33 +0800

    two thread

--- com/lsc/base/Threading.py
+++ com/lsc/base/Threading.py
@@ -5,20 +5,24 @@ Created on 2015年10月15日
 
 @author: lsc
 '''
+
 import time, threading
 
 # 新线程执行的代码:
 def loop():
-    print('thread %s is running...' % threading.current_thread().name)
+    print("thread %s is running..." % threading.current_thread().name)
     n = 0
-    while n < 5:
+    while n < 100:
         n = n + 1
-        print('thread %s >>> %s' % (threading.current_thread().name, n))
-        time.sleep(1)
-    print('thread %s ended.' % threading.current_thread().name)
+        print("thread %s >>> %s" % (threading.current_thread().name, n))
+        #time.sleep(1)
+    print("thread %s ended." % threading.current_thread().name)
 
-print('thread %s is running...' % threading.current_thread().name)
-t = threading.Thread(target=loop, name='LoopThread')
+print("thread %s is running..." % threading.current_thread().name)
+t = threading.Thread(target=loop, name="FirstThread")
+t1 = threading.Thread(target=loop, name="SecondThread")
 t.start()
+t1.start()
 t.join()
-print('thread %s ended.' % threading.current_thread().name)
+t1.join()
+print("thread %s ended." % threading.current_thread().name)
