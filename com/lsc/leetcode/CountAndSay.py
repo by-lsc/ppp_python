1f27540bd64eab340b3fb2aa6d24f03cd49fa0c2
lsc <by-lsc@foxmail.com>
2015-12-23 16:46:39 +0800

    add c&s

---
+++ com/lsc/leetcode/CountAndSay.py
@@ -0,0 +1,12 @@
+'''
+Created on 2015年11月25日
+
+@author: lsc
+@link https://leetcode.com/problems/count-and-say/
+'''
+class Sulution():
+    def countAndSay(self,n):
+        pass
+a = int("12")
+print(a,len(a))
+print("============")
